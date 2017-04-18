d86128a67ba4e8b3cd68e63df3c5d6431d288b83
Keith Chan <keith.chan21@gmail.com>
2017-04-18 16:03:51 -0700

    toggle function complete

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 
 })
 
+// Calculate the hours available for the day
 function calculateTime(day) {
 
 	var From = $('#' + day + 'From')[0].value;
@@ -15,6 +16,11 @@ function calculateTime(day) {
 		From = From.split(":");
 		To = To.split(":");
 
+		// if toggle is flipped to PM, add 12 to hours
+		if($('#' + day + 'FromToggle').is(":checked")) {
+			From[0] = parseInt(From[0]) + 12;
+		}
+
 		var fromDate = new Date(0, 0, 0, From[0], From[1] || 0, 0);
 		var toDate = new Date(0, 0, 0, To[0], To[1] || 0, 0);
 
@@ -24,11 +30,8 @@ function calculateTime(day) {
 		// Make sure time difference is positive, else 0
 		if (diff < 0) diff = 0;
 
-		var hours = Math.floor(diff / 1000 / 60 / 60);
-		diff -= hours * 1000 * 60 * 60;
-		var minutes = Math.floor(diff / 1000 / 60);
-
-		$('#' + day + 'Total').text((hours < 9 ? "0" : "") + hours + ":" + (minutes < 9 ? "0" : "") + minutes)
+		var hours = diff/ 1000/ 60/ 60;
+		$('#' + day + 'Total').text(round(hours, 2));
 		calculateTotalAvailability();
 	}
 	// if both fields are not filled in
@@ -40,25 +43,22 @@ function calculateTime(day) {
 
 }
 
+// Calculate hours available for the week
 function calculateTotalAvailability() {
 
 	// Array of all dates in the week
 	var days = ["Monday", "Tuesday"];
 	var totalHours = 0;
-	var standard = new Date(0, 0, 0, 0, 0, 0);
 
 	// Loop to add up all hours
 	for(count = 0; count < days.length; count++) {
 		var text = $('#' + days[count] + 'Total').html();
-		text = text.split(":");
-		var dailyHours = new Date(0, 0, 0, text[0], text[1] || 0, 0);
-		totalHours += dailyHours.getTime() - standard.getTime();
+		totalHours += parseFloat(text);
 	}
 
-	var hours =  Math.floor(totalHours / 1000 / 60 / 60);
-	totalHours -= hours * 1000 * 60 * 60;
-	var minutes = Math.floor(totalHours / 1000 / 60);
+	$('#Total').text(totalHours);
+}
 
-	// Change text of total availability 
-	$('#Total').text((hours < 9 ? "0" : "") + hours + ":" + (minutes < 9 ? "0" : "") + minutes);
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
