611d119159dc285d5e1c982acc2423c8df3b9097
Keith Chan <keith.chan21@gmail.com>
2017-04-19 11:38:44 -0700

    second field and all toggles complete

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
 
+	console.log("Ready");
 
 })
 
 // Calculate the hours available for the day
 function calculateTime(day) {
 
+	// variables for hours available
+	var available = 0;
+	var available1 = 0;
+
+	// first availability slot
 	var From = $('#' + day + 'From')[0].value;
 	var To = $('#' + day + 'To')[0].value;
 
-	// if both fields are filled in
+	// second availability slot
+	var From1 = $('#' + day + 'From1')[0].value;
+	var To1 = $('#' + day + 'To1')[0].value;
+
+	// if both fields in first availability slot are filled in
 	if(From != 0 && To != 0) {
 
 		// split time into two parts, hours and minutes
@@ -19,6 +29,9 @@ function calculateTime(day) {
 		// if toggle is flipped to PM, add 12 to hours
 		if($('#' + day + 'FromToggle').is(":checked")) {
 			From[0] = parseInt(From[0]) + 12;
+		}
+		if($('#' + day + 'ToToggle').is(":checked")) {
+			To[0] = parseInt(To[0]) + 12;
 		}
 
 		var fromDate = new Date(0, 0, 0, From[0], From[1] || 0, 0);
@@ -31,15 +44,37 @@ function calculateTime(day) {
 		if (diff < 0) diff = 0;
 
 		var hours = diff/ 1000/ 60/ 60;
-		$('#' + day + 'Total').text(round(hours, 2));
-		calculateTotalAvailability();
+		available = round(hours, 2);
+		//$('#' + day + 'Total').text(round(hours, 2));
+		//calculateTotalAvailability();
 	}
-	// if both fields are not filled in
-	else {
-		// hours available = 0
-		$('#' + day + 'Total').text(0);
-		calculateTotalAvailability();
+
+	// if second availability slot is filled in
+	if(From1 != 0 && To1 != 0) {
+
+		From1 = From1.split(":");
+		To1 = To1.split(":");
+
+		if($('#' + day + 'From1Toggle').is(":checked")) {
+			From1[0] = parseInt(From1[0]) + 12;
+		}
+		if($('#' + day + 'To1Toggle').is(":checked")) {
+			To1[0] = parseInt(To1[0]) + 12;
+		}
+
+		var from1Date = new Date(0, 0, 0, From1[0], From1[1] || 0, 0);
+		var to1Date = new Date(0, 0, 0, To1[0], To1[1] || 0, 0);
+
+		var diff1 = to1Date.getTime() - from1Date.getTime();
+
+		if(diff1 < 0) diff1 = 0;
+
+		var hours1 = diff1/ 1000 / 60 / 60;
+		available1 = round(hours1, 2);
 	}
+
+	$('#' + day + 'Total').text(available + available1);
+	calculateTotalAvailability();
 
 }
 
